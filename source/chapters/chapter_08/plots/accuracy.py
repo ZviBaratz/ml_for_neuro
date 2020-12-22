c496640d7415b79f5e384e62b2770b2b05e9322e
Zvi Baratz <z.baratz@gmail.com>
2020-12-22 19:42:01 +0200

    Fixed customizations in accuracy plot.

--- source/chapters/chapter_08/plots/accuracy.py
+++ source/chapters/chapter_08/plots/accuracy.py
@@ -20,6 +20,7 @@ def plot_accuracy_by_pc(
     true_labels: np.ndarray,
     fig_kwargs: dict = None,
     ax_kwargs: dict = None,
+    customizations: dict = None,
 ) -> Tuple[plt.Figure, plt.Axes]:
     """
     Plot accuracy scores by number of principal components.
@@ -43,6 +44,7 @@ def plot_accuracy_by_pc(
     """
     fig_kwargs = organize_kwargs(fig_kwargs, DEFAULT_FIGURE_KWARGS)
     ax_kwargs = organize_kwargs(ax_kwargs, DEFAULT_AXES_KWARGS)
+    customizations = organize_kwargs(customizations, DEFAULT_CUSTOMIZATIONS)
     distinct_labels = sorted(set(true_labels))
     scores = []
     for labels_array in classification_labels:
@@ -58,5 +60,5 @@ def plot_accuracy_by_pc(
     fig, ax = plt.subplots(**fig_kwargs)
     x_range = range(1, len(scores) + 1)
     ax.plot(x_range, scores, **ax_kwargs)
-    ax.set(xticks=x_range, xticklabels=x_range)
+    ax.set(xticks=x_range, xticklabels=x_range, **customizations)
     return fig, ax
